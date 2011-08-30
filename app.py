5705408c0d0641fe0710f4c0ba746bebf6af8d18
Sridhar Ratnakumar <sridharr@activestate.com>
2011-08-30 11:36:08 -0700

    use VCAP_APP_HOST in the python samples that do not use uWSGI
    cloudfoundry requires that applications bind to a specified host
    (not always localhost)

--- app.py
+++ app.py
@@ -10,5 +10,11 @@ class HelloWorld:
 
 
 if __name__ == '__main__':
-    port = int(os.getenv('VMC_APP_PORT', '8001'))
-    cherrypy.quickstart(HelloWorld(), config={'global': {'server.socket_port': port}})
+    port = int(os.getenv('VCAP_APP_PORT', '8000'))
+    host = os.getenv('VCAP_APP_HOST', '0.0.0.0')
+    cherrypy.quickstart(
+        HelloWorld(),
+        config={
+            'global': {
+                'server.socket_port': port,
+                'server.socket_host': host}})
